9a7f61a3a883ab26aa71c766c09e4ef842c39c09
Mohamed Ashour <m.aly.ashour@gmail.com>
2025-06-29 20:52:18 +0200

    Implement satellite server and visualization interface

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"os"
 
 	"github.com/Mohammed-Ashour/tlego/cmd"
+	"github.com/Mohammed-Ashour/tlego/pkg/server"
 )
 
 func main() {
 	cmd.RootCmd.Run(context.Background(), os.Args)
-
+	server.StartServer()
 }
